5f924db3c724f6c480c632252f3f24f698e51872
mehrdad <=>
2022-12-06 15:15:00 +0330

    comment view and form changes

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .models import Comment
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
@@ -6,3 +8,7 @@ class EmailPostForm(forms.Form):
     comments = forms.CharField(required=False,
                               widget=forms.Textarea)
 
+class CommentForm(forms.ModelForm):
+    class meta:
+        model = Comment
+        fields = ('name','email','body')
