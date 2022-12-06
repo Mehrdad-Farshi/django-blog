5c266face828afe7dbeaef34667c3f2918c0b53e
mehrdad <=>
2022-12-06 11:55:56 +0330

    model of comment

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,20 @@ class Post (models.Model):
 
     def __str__ (self) :
         return self.title
+
+class comment(models.Model):
+    post = models.ForeignKey(Post,
+                            on_delete = models.CASCADE,
+                            related_name = 'comments',)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    active = models.BooleanField(default=True)
+
+    class meta :
+        ordering = ('created',)
+    
+    def __str__(self) :
+        return f'comment by {self.name} on {self.post}'
